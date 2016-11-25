68d9d12120b6c542d31446766bb1f27a1ed4facc
Mathias Ettinger <mathias.ettinger@toulouse.viveris.com>
2016-11-25 11:44:46 +0100

    Prettyfied imports

--- data_access/__init__.py
+++ data_access/__init__.py
@@ -1,7 +1,22 @@
 from .result_requester import Requester
-from .result_filter import OperandStatistic, OperandTimestamp, OperandValue
-from .result_filter import ConditionAnd, ConditionOr, ConditionEqual
-from .result_filter import ConditionUpperOrEqual, ConditionUpper, ConditionNotEqual
-from .result_filter import ConditionBelow, ConditionBelowOrEqual, Filter
-from .result_data import ScenarioInstanceResult, AgentResult, JobInstanceResult
-from .result_data import StatisticResult, StatisticValue
+from .result_filter import (
+        OperandStatistic,
+        OperandTimestamp,
+        OperandValue,
+        ConditionAnd,
+        ConditionOr,
+        ConditionEqual,
+        ConditionUpperOrEqual,
+        ConditionUpper,
+        ConditionNotEqual,
+        ConditionBelow,
+        ConditionBelowOrEqual,
+        Filter,
+)
+from .result_data import (
+        ScenarioInstanceResult,
+        AgentResult,
+        JobInstanceResult,
+        StatisticResult,
+        StatisticValue,
+)
