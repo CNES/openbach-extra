18532cb52936c3c6c262dd75404bc04a1080977d
Mathias Ettinger <mathias.ettinger@toulouse.viveris.com>
2016-11-25 14:55:15 +0100

    Added documentation

--- scenario_builder/core.py
+++ scenario_builder/core.py
@@ -1,8 +1,19 @@
+import json
 from .openbach_functions import OpenBachFunction
 
 
 class Scenario:
+    """Interface between Python code and JSON scenario definition.
+
+    This class aims at reducing the boilerplate necessary when
+    defining configuration files for OpenBach scenarios. Instances
+    of this class represents a configuration file for a given
+    scenario.
+    """
+
     def __init__(self, name, description=None):
+        """Construct an empty scenario with the given metadata"""
+
         if description is None:
             description = name
 
@@ -13,24 +24,41 @@ class Scenario:
         self.openbach_functions = []
 
     def add_argument(self, name, description):
+        """Add an argument for this scenario"""
+
         self.arguments[name] = description
 
     def remove_argument(self, name):
+        """Remove the given argument from this scenario"""
+
         try:
             del self.arguments[name]
         except KeyError:
             pass
 
     def add_constant(self, name, value):
+        """Associate a value to a constant for this scenario"""
+
         self.constants[name] = value
 
     def remove_constants(self, name):
+        """Remove the given constant from this scenario"""
+
         try:
             del self.contants[name]
         except KeyError:
             pass
 
     def add_function(self, name, wait_delay=0, wait_launched=None, wait_finished=None):
+        """Add an openbach function to this scenario.
+
+        The function will be started after all other functions in
+        `wait_launched` are started and all other functions in
+        `wait_finished` are completed. After these conditions are
+        met (if any), an additional delay of `wait_delay` will be
+        applied before the function starts.
+        """
+
         wait_launched = check_and_build_waiting_list(wait_launched)
         wait_finished = check_and_build_waiting_list(wait_finished)
 
@@ -41,12 +69,24 @@ class Scenario:
         return function
 
     def remove_function(self, function):
+        """Remove the given function from this scenario.
+
+        The function should be an object returned by
+        `self.add_function(...)`.
+        """
+
         try:
             self.openbach_functions.remove(function)
         except ValueError:
             pass
 
     def build(self):
+        """Construct a dictionary representing this scenario.
+
+        This dictionary is suitable to be written in a file as
+        JSON data.
+        """
+
         openbach_functions = self.openbach_functions
         functions = [f.build(openbach_functions) for f in openbach_functions]
         return {
@@ -57,8 +97,22 @@ class Scenario:
             'openbach_functions': functions,
         }
 
+    def write(self, filename):
+        """Write the JSON representation of this scenario in
+        the requested file.
+        """
+
+        with open(filename, 'w') as fp:
+            json.dump(self.build(), fp)
+
 
 def check_and_build_waiting_list(wait_on=None):
+    """Check that each element container in the `wait_on` iterable
+    is a proper openbach function. Raise `TypeError` otherwise.
+
+    Return the `wait_on` iterable converted to a list.
+    """
+
     wait_on = [] if wait_on is None else list(wait_on)
     if not all(isinstance(obj, OpenBachFunction) for obj in wait_on):
         raise TypeError('can only wait on iterables of openbach functions')
@@ -66,6 +120,8 @@ def check_and_build_waiting_list(wait_on=None):
 
 
 def get_function_factory(name):
+    """Convert a name to the appropriate openbach function"""
+
     for cls in OpenBachFunction.__subclasses__():
         if cls.__name__ == name:
             return cls
--- scenario_builder/openbach_functions.py
+++ scenario_builder/openbach_functions.py
@@ -1,32 +1,55 @@
 class ImproperlyConfiguredFunction(Exception):
+    """Exception raised when an openbach function that has not been
+    configured yet is used to build a scenario.
+    """
+
     def __init__(self, name):
         super().__init__('{} has not been configured yet'.format(name))
 
 
 class OpenBachFunction:
+    """Base class for every OpenBach functions.
+
+    Define the common boilerplate necessary to build their
+    representation.
+    """
+
     def __init__(self, launched, finished, delay):
-        self.wait = {
-            'time': delay,
-            'launched_indexes': launched,
-            'finished_indexes': finished,
-        }
+        self.time = delay
+        self.wait_launched = launched
+        self.wait_finished = finished
 
     def build(self, functions):
-        context = {'wait': {'time': self.wait['time']}}
-        context['wait']['launched_indexes'] = list(
-                safe_indexor(functions, self.wait['launched_indexes']))
-        context['wait']['finished_indexes'] = list(
-                safe_indexor(functions, self.wait['finished_indexes']))
+        """Construct a dictionary representing this function.
+
+        This dictionary is suitable to be included in the
+        `openbach_functions` array of the associated scenario.
+        """
+
+        context = {'wait': {'time': self.time}}
+        context['wait']['launched_indexes'] = list(safe_indexor(functions, self.wait_launched))
+        context['wait']['finished_indexes'] = list(safe_indexor(functions, self.wait_finished))
         return context
 
 
 class StartJobInstance(OpenBachFunction):
+    """Representation of the start_job_instance openbach function."""
+
     def __init__(self, launched, finished, delay):
         super().__init__(launched, finished, delay)
         self.start_job_instance = {}
         self.job_name = None
 
     def configure(self, job_name, agent_ip, offset=0, **job_arguments):
+        """Define this openbach function with the mandatory values:
+         - job_name: name of the job to start;
+         - agent_ip: address of the agent that should start the job;
+         - offset: delay for the scheduler on the agent before starting
+                   the job;
+         - job_arguments: key=value pairs of arguments provided to
+                          the job on the command line when starting it.
+        """
+
         arguments = self.start_job_instance
         arguments.clear()
         arguments['agent_ip'] = agent_ip
@@ -35,6 +58,12 @@ class StartJobInstance(OpenBachFunction):
         self.job_name = job_name
 
     def build(self, functions):
+        """Construct a dictionary representing this function.
+
+        This dictionary is suitable to be included in the
+        `openbach_functions` array of the associated scenario.
+        """
+
         if self.job_name is None:
             raise ImproperlyConfiguredFunction('start_job_instance')
 
@@ -52,14 +81,26 @@ class StartJobInstance(OpenBachFunction):
 
 
 class StopJobInstance(OpenBachFunction):
+    """Representation of the stop_job_instance openbach function."""
+
     def __init__(self, launched, finished, delay):
         super().__init__(launched, finished, delay)
         self.openbach_function_indexes = ()
 
     def configure(self, *functions):
+        """Define this openbach function with the mandatory values:
+         - functions: openbach functions to stop.
+        """
+
         self.openbach_function_indexes = functions
 
     def build(self, functions):
+        """Construct a dictionary representing this function.
+
+        This dictionary is suitable to be included in the
+        `openbach_functions` array of the associated scenario.
+        """
+
         context = super().build(functions)
         context['stop_job_instance'] = {
             'openbach_function_indexes': list(
@@ -68,6 +109,12 @@ class StopJobInstance(OpenBachFunction):
 
 
 def safe_indexor(reference, lookup):
+    """Generate the index of each element of `lookup` in the
+    `reference` array.
+
+    Skip missing elements.
+    """
+
     for element in lookup:
         try:
             yield reference.index(element)
