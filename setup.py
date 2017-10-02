6d1f9a1663af10e1bfce9151b039fbd9cb025c4e
Mathias Ettinger <mathias.ettinger@toulouse.viveris.com>
2017-10-02 16:57:55 +0200

    Bump version + add README

--- setup.py
+++ setup.py
@@ -4,23 +4,21 @@
 import os
 from setuptools import setup, find_packages
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
+
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-    
+    with open(os.path.join(os.path.dirname(__file__), fname)) as readme:
+        return readme.read()
+
+
 setup(
-    name="openbach-api",
-    version="1.0.1",
-    author="OpenBACH Team",
-    author_email="admin@openbach.org",
-    description=("OpenBACH API to build scenario JSONs and to access" \
-                 "stats/log of the Collector data"),
-    license="GPL",
-    url="http://openbach.org",
-    
+    name='openbach-api',
+    version='1.1.0',
+    author='OpenBACH Team',
+    author_email='admin@openbach.org',
+    description='OpenBACH API: build scenario JSONs and access Collector Data',
+    long_description=read('README.md'),
+    license='GPL',
+    url='http://openbach.org',
+
     packages=find_packages()
 )
-
