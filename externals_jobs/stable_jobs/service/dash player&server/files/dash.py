1e34b85dfecec0c4defeaf38ad265032a138be0d
David Pradas <david.pradas@toulouse.viveris.com>
2019-02-04 16:30:33 +0100

    force firefox version and remove address parameter on dash server&player

--- externals_jobs/stable_jobs/service/dash player&server/files/dash.py
+++ externals_jobs/stable_jobs/service/dash player&server/files/dash.py
@@ -72,7 +72,7 @@ class CustomWebSocket(websocket.WebSocketHandler):
         collect_agent.send_log(syslog.LOG_DEBUG, 'Message received')
 
 
-def main(address, port):
+def main(port):
     # Connect to collect_agent
     success = collect_agent.register_collect(
             '/opt/openbach/agent/jobs/dash_player_server/'
@@ -92,10 +92,10 @@ def main(address, port):
         }),
     ])
 
-    listen_message = 'Starting tornado on {}:{}'.format(address, port)
+    listen_message = 'Starting tornado on {}:{}'.format('0.0.0.0', port)
     print(listen_message)
     collect_agent.send_log(syslog.LOG_DEBUG, listen_message)
-    application.listen(port, address)
+    application.listen(port, '0.0.0.0')
     ioloop.IOLoop.current().start()
 
 
@@ -107,9 +107,6 @@ if __name__ == '__main__':
     parser.add_argument(
             '-p', '--port', type=int, default=80,
             help='Port to use to serve HTTP')
-    parser.add_argument(
-            '-a', '--address', type=str, default='0.0.0.0',
-            help='Address to bind to')
 
     # get args
     args = parser.parse_args()
