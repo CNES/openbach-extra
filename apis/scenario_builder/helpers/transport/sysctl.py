8adcf049743af28d494ceb4be4c9e3a6e48b3523
David Pradas <david.pradas@toulouse.viveris.com>
2019-06-19 18:03:16 +0200

    New helper sysctl

--- apis/scenario_builder/helpers/transport/sysctl.py
+++ apis/scenario_builder/helpers/transport/sysctl.py
@@ -42,3 +42,18 @@ def sysctl_configure_tcp_congestion_control(
             parameter='net.ipv4.tcp_congestion_control',
             value=congestion_control)
     return function
+
+
+def sysctl(
+        scenario, entity, param, value,
+        wait_finished=None, wait_launched=None, wait_delay=0):
+    function = scenario.add_function(
+            'start_job_instance',
+            wait_finished=wait_finished,
+            wait_launched=wait_launched,
+            wait_delay=wait_delay)
+    function.configure(
+            'sysctl', entity,
+            parameter=param,
+            value=value)
+    return function
