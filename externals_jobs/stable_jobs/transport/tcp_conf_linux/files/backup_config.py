a19aa42014b6989823ac79945e4e10b83dad97b1
Bastien Tauran <bastien.tauran@viveris.fr>
2019-07-01 11:20:00 +0200

    job tcp_conf_linux: adding headers, and changing help file

--- externals_jobs/stable_jobs/transport/tcp_conf_linux/files/backup_config.py
+++ externals_jobs/stable_jobs/transport/tcp_conf_linux/files/backup_config.py
@@ -1,3 +1,34 @@
+#!/usr/bin/env python3
+
+# OpenBACH is a generic testbed able to control/configure multiple
+# network/physical entities (under test) and collect data from them. It is
+# composed of an Auditorium (HMIs), a Controller, a Collector and multiple
+# Agents (one for each network entity that wants to be tested).
+#
+#
+# Copyright © 2016 CNES
+#
+#
+# This file is part of the OpenBACH testbed.
+#
+#
+# OpenBACH is a free software : you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY, without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see http://www.gnu.org/licenses/.
+
+
+"""Copy the default configuration in /opt/openbach/agent/jobs/tcp_conf_linux/
+at job installation"""
+
 dst = open("/opt/openbach/agent/jobs/tcp_conf_linux/default_tcp_conf_linux.conf","w")
 for param in ["tcp_congestion_control", "tcp_slow_start_after_idle", 
 		"tcp_no_metrics_save", "tcp_sack", "tcp_recovery", "tcp_wmem", "tcp_fastopen"]:
