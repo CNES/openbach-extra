1362b19e984798b5d7d5dd7859e38db1646a23f1
Francklin Simo <francklin.simo@toulouse.viveris.com>
2019-07-16 12:15:32 +0200

    Add helper ip_route

---
+++ apis/scenario_builder/helpers/network/ip_route.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#   OpenBACH is a generic testbed able to control/configure multiple
+#   network/physical entities (under test) and collect data from them. It is
+#   composed of an Auditorium (HMIs), a Controller, a Collector and multiple
+#   Agents (one for each network entity that wants to be tested).
+#
+#
+#   Copyright © 2016−2019 CNES
+#
+#
+#   This file is part of the OpenBACH testbed.
+#
+#
+#   OpenBACH is a free software : you can redistribute it and/or modify it under
+#   the terms of the GNU General Public License as published by the Free Software
+#   Foundation, either version 3 of the License, or (at your option) any later
+#   version.
+#
+#   This program is distributed in the hope that it will be useful, but WITHOUT
+#   ANY WARRANTY, without even the implied warranty of MERCHANTABILITY or FITNESS
+#   FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+#   details.
+#
+#   You should have received a copy of the GNU General Public License along with
+#   this program. If not, see http://www.gnu.org/licenses/.
+
+""" Helpers of ip_route job """
+
+def ip_route(
+       scenario, entity, operation, destination_ip, gateway_ip=None, 
+       device=None, initcwnd=0, initrwnd=0, wait_finished=None, 
+       wait_launched=None, wait_delay=0):
+    route_config = scenario.add_function(
+                'start_job_instance',
+                wait_finished=wait_finished,
+                wait_launched=wait_launched,
+                wait_delay=wait_delay
+    )
+    
+    parameters={
+                'operation': operation,
+                'offset': 0,
+                'initcwnd': initcwnd,
+                'initrwnd': initrwnd
+    }
+    if gateway_ip:
+       parameters['gateway_ip']=gateway_ip
+    if device:
+       parameters['device']=device
+    if destination_ip == 'default':
+       parameters['default']={}
+    else:
+       parameters['destination_ip']={'network_ip':destination_ip}    
+
+    route_config.configure('ip_route', entity, **parameters)
+
+    return [route_config]
