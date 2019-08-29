20608dff3fca26b08e2784f24afe067b8720d462
fsimo <fsimo@toulouse.viveris.com>
2019-08-29 16:32:14 +0200

    Add helper apache2

---
+++ apis/scenario_builder/helpers/service/apache2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#   OpenBACH is a generic testbed able to control/configure multiple
+#   network/physical entities (under test) and collect data from them. It is
+#   composed of an Auditorium (HMIs), a Controller, a Collector and multiple
+#   Agents (one for each network entity that wants to be tested).
+#
+#
+#   Copyright © 2016−2019 CNES
+#
+#
+#   This file is part of the OpenBACH testbed.
+#
+#
+#   OpenBACH is a free software : you can redistribute it and/or modify it under
+#   the terms of the GNU General Public License as published by the Free Software
+#   Foundation, either version 3 of the License, or (at your option) any later
+#   version.
+#
+#   This program is distributed in the hope that it will be useful, but WITHOUT
+#   ANY WARRANTY, without even the implied warranty of MERCHANTABILITY or FITNESS
+#   FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+#   details.
+#
+#   You should have received a copy of the GNU General Public License along with
+#   this program. If not, see http://www.gnu.org/licenses/.
+
+""" Helpers of apache2 job """
+
+def apache2(
+       scenario, server_entity, start_stop, wait_finished=None, 
+       wait_launched=None, wait_delay=0):
+    launch_apache2 = scenario.add_function(
+                         'start_job_instance',
+                         wait_finished=wait_finished,
+                         wait_launched=wait_launched,
+                         wait_delay=wait_delay)
+    launch_apache2.configure(
+                       'apache2', server_entity, offset=0,
+                       operation=start_stop)
+
+    return [launch_apache2]
