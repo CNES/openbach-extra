984bba6749e81a0074134c0f13ac24e5b2fc1882
Francklin Simo <david.pradas@toulouse.viveris.com>
2019-11-05 11:02:37 +0100

    Add reference scenario for configure link

---
+++ apis/scenario_builder/scenarios/configure_link.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#   OpenBACH is a generic testbed able to control/configure multiple
+#   network/physical entities (under test) and collect data from them. It is
+#   composed of an Auditorium (HMIs), a Controller, a Collector and multiple
+#   Agents (one for each network entity that wants to be tested).
+#
+#
+#   Copyright © 2016−2019 CNES
+#
+#
+#   This file is part of the OpenBACH testbed.
+#
+#
+#   OpenBACH is a free software : you can redistribute it and/or modify it under
+#   the terms of the GNU General Public License as published by the Free Software
+#   Foundation, either version 3 of the License, or (at your option) any later
+#   version.
+#
+#   This program is distributed in the hope that it will be useful, but WITHOUT
+#   ANY WARRANTY, without even the implied warranty of MERCHANTABILITY or FITNESS
+#   FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+#   details.
+#
+#   You should have received a copy of the GNU General Public License along with
+#   this program. If not, see http://www.gnu.org/licenses/.
+
+from scenario_builder import Scenario
+from scenario_builder.helpers.network.configure_link import configure_link_apply
+from scenario_builder.helpers.network.configure_link import configure_link_clear
+from inspect import signature
+
+
+SCENARIO_DESCRIPTION="""This scenario allows to:
+     - Apply/Clear a configuration on network interfaces in ingress, egress or both directions 
+       in order to emulate/stop emulation of a network link like WIFI link. 
+       Many link characteristiscs can be emulated including: bandwidth, delay, jitter and losses
+"""
+SCENARIO_NAME="""configure_link"""
+
+
+def build(entity, ifaces, mode, operation, bandwidth=None, delay=0, jitter=0,
+          delay_distribution='normal', loss_model='random', loss_model_params=[0.0], 
+          scenario_name=SCENARIO_NAME):
+
+    # Create scenario and add scenario arguments if needed
+    scenario = Scenario(scenario_name, SCENARIO_DESCRIPTION)
+    args = signature(build).parameters
+    for arg in args:
+        value = locals()[arg] 
+        if str(value).startswith('$'):
+           scenario.add_argument(value[1:], '')
+
+    if operation == 'apply':
+       configure_link_apply(scenario, entity, ifaces, mode, bandwidth, delay_distribution,
+                            delay, jitter, loss_model, loss_model_params)
+    else:
+       configure_link_clear(scenario, entity, ifaces, mode)
+
+    return scenario
+
+
+
---
+++ reference_scenarios/generate_configure_link.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# OpenBACH is a generic testbed able to control/configure multiple
+# network/physical entities (under test) and collect data from them.
+# It is composed of an Auditorium (HMIs), a Controller, a Collector
+# and multiple Agents (one for each network entity that wants to be
+# tested).
+#
+#
+# Copyright © 2016-2019 CNES
+#
+#
+# This file is part of the OpenBACH testbed.
+#
+#
+# OpenBACH is a free software : you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY, without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+
+"""This scenario builds and launches the *configure_link* scenario
+from /openbach-extra/apis/scenario_builder/scenarios/
+"""
+
+
+from auditorium_scripts.scenario_observer import ScenarioObserver
+from scenario_builder.scenarios import configure_link
+
+
+def main(scenario_name='configure_link', argv=None):
+    observer = ScenarioObserver()
+    observer.add_scenario_argument(
+            '--entity', '--entity', required=True,
+            help='Name of the entity emulating the link')
+    observer.add_scenario_argument(
+            '--ifaces', '--network_interfaces', required=True,
+            help='Commat-separed list of the network interfaces to emulate link on')
+    observer.add_scenario_argument(
+            '--mode', choices=['ingress', 'egress', 'all'], required=True,
+            help='Targeted direction: ingress, egress or all')
+    observer.add_scenario_argument(
+            '--operation', choices=['apply', 'clear'], required=True, 
+            help='Choose apply to add configuration or clear to delete existing ones')
+    observer.add_scenario_argument(
+            '--bandwidth', type=str, 
+            help='The link bandwidth in Mbps or Kbps, expressed as [value][M|K] '
+                  '(only for apply operation)')
+    observer.add_scenario_argument(
+            '--delay', type=int, default=0, 
+            help='Delay to add to packets, in ms (only for apply operation)')
+    observer.add_scenario_argument(
+            '--jitter', type=int, default=0, 
+            help='Delay variation, in ms (only for apply operation)')
+    observer.add_scenario_argument(
+            '--dd', '--delay_distribution', 
+            choices=['uniform', 'normal', 'pareto', 'paretonormal'], default='normal', 
+            help='Distribution to use to choose delay value (only for apply operation)')
+    observer.add_scenario_argument(
+            '--lm', '--loss_model', choices=['random', 'state', 'gemodel'], 
+            default='random', help='Packets loss model to use (only for apply operation)')
+    observer.add_scenario_argument(
+            '--lmp', '--loss_model_paramaters', default=0.0, type=float, nargs='*',
+            help='Packets loss model to use (only for apply operation)')
+    
+    
+    args = observer.parse(argv, scenario_name)
+
+    scenario = configure_link.build(
+                args.entity,
+                args.ifaces,
+                args.mode,
+                args.operation,
+                args.bandwidth,
+                args.delay,
+                args.jitter,
+                args.dd,
+                args.lm,
+                args.lmp,
+    )
+    observer.launch_and_wait(scenario)
+
+
+if __name__ == '__main__':
+    main()
