3439b3bc4051102ae6f2d5683d464dc9a26c9f32
Aurélien Delrieu <aurelien.delrieu@viveris.fr>
2020-01-15 10:27:11 +0000

    Add IP forwarding to OpenSAND Job (IP network configuration)

--- externals_jobs/stable_jobs/access/opensand/files/opensand.py
+++ externals_jobs/stable_jobs/access/opensand/files/opensand.py
@@ -407,6 +407,19 @@ if __name__ == '__main__':
 
                 onu.add_address(args.bridge_name, args.address_mask)
 
+                try:
+                    ipaddress.IPv4Network(args.address_mask, False)
+                    onu.enable_ipv4_forward()
+                    onu.enable_ipv4_forwarding(args.bridge_name)
+                except ipaddress.AddressValueError:
+                    pass
+                try:
+                    ipaddress.IPv6Network(args.address_mask, False)
+                    onu.enable_ipv6_forward()
+                    onu.enable_ipv6_forwarding(args.bridge_name)
+                except ipaddress.AddressValueError:
+                    pass
+              
             elif args.action == onu.ETH_TYPE:
                 onu.create_tap_iface(args.tap_name)
                 onu.set_up(args.tap_name)
