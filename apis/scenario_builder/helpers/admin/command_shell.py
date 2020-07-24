40aead5caf466486a74e93f43ccad1bac222d996
Bastien Tauran <bastien.tauran@viveris.fr>
2020-07-24 16:21:03 +0200

    Fix command_shell helper

--- apis/scenario_builder/helpers/admin/command_shell.py
+++ apis/scenario_builder/helpers/admin/command_shell.py
@@ -32,11 +32,11 @@
 def command_shell(
         scenario, entity, command,
         wait_finished=None, wait_launched=None, wait_delay=0):
-    command = scenario.add_function(
+    function = scenario.add_function(
             'start_job_instance',
             wait_finished=wait_finished,
             wait_launched=wait_launched,
             wait_delay=wait_delay)
-    command.configure('command_shell', entity, command_line=command)
+    function.configure('command_shell', entity, command_line=command)
 
-    return [command]
+    return [function]
