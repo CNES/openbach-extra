e361bf109a867946cc0588e830e44fc329c2d99d
Mathias Ettinger <mathias.ettinger@viveris.f>
2020-09-23 15:50:02 +0200

    Implementing sub-commands supports for start_job_instance
      Closes #45

--- apis/auditorium_scripts/start_job_instance.py
+++ apis/auditorium_scripts/start_job_instance.py
@@ -38,14 +38,59 @@ __credits__ = '''Contributors:
 
 
 import shlex
+import argparse
 from functools import partial
 
 from auditorium_scripts.frontend import FrontendBase, DEFAULT_DATE_FORMAT
 
 
-def parse(value):
-    name, *values = shlex.split(value, posix=True)
-    return name, values
+class Argument(argparse.Action):
+    PARENTS = 0
+
+    def __call__(self, parser, args, values, option_string=None):
+        items = getattr(args, self.dest)
+        if items is None:
+            items = {}
+            setattr(args, self.dest, items)
+
+        self._parse_command_line(items, values)
+
+    def _parse_command_line(self, items, values):
+        for _ in range(self.PARENTS):
+            try:
+                subcommand, *arguments = values
+            except ValueError as e:
+                raise argparse.ArgumentError(self, e)
+            else:
+                items = items.setdefault(subcommand, {})
+                values = arguments
+
+        try:
+            name, *values = values
+        except ValueError as e:
+            raise argparse.ArgumentError(self, e)
+
+        items[name] = values
+
+
+class SubCommand(Argument):
+    PARENTS = 1
+
+
+class SubSubCommand(Argument):
+    PARENTS = 2
+
+
+class SubSubSubCommand(Argument):
+    PARENTS = 3
+
+
+class SubSubSubSubCommand(Argument):
+    PARENTS = 4
+
+
+class SubSubSubSubSubCommand(Argument):
+    PARENTS = 5
 
 
 class StartJobInstance(FrontendBase):
@@ -54,9 +99,28 @@ class StartJobInstance(FrontendBase):
         self.parser.add_argument('agent_address', help='IP address or domain of the agent')
         self.parser.add_argument('job_name', help='name of the job to start')
         self.parser.add_argument(
-                '-a', '--argument', type=parse, nargs='+', default={},
-                metavar='NAME[ VALUE[ VALUE...]]',
-                help='')
+                '-a', '--argument', nargs='+', action=Argument, metavar=('NAME', 'VALUE'),
+                help='description of an argument with its name and associated value(s)')
+        self.parser.add_argument(
+                '-s', '--sub-command', dest='argument', nargs='+', action=SubCommand,
+                metavar=('SUBCOMMAND', 'ARGUMENT_DESCRIPTION'),
+                help='description of sub-command with its name and associated argument description (name and value(s), see "-a")')
+        self.parser.add_argument(
+                '--sub-sub-command', dest='argument', nargs='+', action=SubSubCommand,
+                metavar=('SUBSUBCOMMAND', 'SUBCOMMAND_DESCRIPTION'),
+                help='description of sub-sub-command with its name and associated sub-command description (name and argument, see "-s")')
+        self.parser.add_argument(
+                '--sub-sub-sub-command', dest='argument', nargs='+', action=SubSubSubCommand,
+                metavar=('SUBSUBSUBCOMMAND', 'SUBSUBCOMMAND_DESCRIPTION'),
+                help='description of sub-sub-sub-command with its name and associated sub-sub-command description')
+        self.parser.add_argument(
+                '--sub-sub-sub-sub-command', dest='argument', nargs='+', action=SubSubSubSubCommand,
+                metavar=('SUBSUBSUBSUBCOMMAND', 'SUBSUBSUBCOMMAND_DESCRIPTION'),
+                help='description of sub-sub-sub-sub-command with its name and associated sub-sub-sub-command description')
+        self.parser.add_argument(
+                '--sub-sub-sub-sub-sub-command', dest='argument', nargs='+', action=SubSubSubSubSubCommand,
+                metavar=('SUBSUBSUBSUBSUBCOMMAND', 'SUBSUBSUBSUBCOMMAND_DESCRIPTION'),
+                help='description of sub-sub-sub-sub-sub-command with its name and associated sub-sub-sub-sub-command description')
         group = self.parser.add_mutually_exclusive_group(required=False)
         group.add_argument(
                 '-d', '--date', metavar=('DATE', 'TIME'), nargs=2,
@@ -70,7 +134,7 @@ class StartJobInstance(FrontendBase):
     def execute(self, show_response_content=True):
         agent = self.args.agent_address
         job_name = self.args.job_name
-        arguments = dict(self.args.argument)
+        arguments = self.args.argument
         date = self.date_to_timestamp()
         interval = self.args.interval
 
