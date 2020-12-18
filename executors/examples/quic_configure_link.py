5f015b143018e380103b6fe343edbcb756079d23
Francklin SIMO <user.email=armelfrancklin.simotegueu@viveris.fr>
2020-12-18 13:25:11 +0000

    Fix regression bug caused by the update of the tcpdump job

--- executors/examples/quic_configure_link.py
+++ executors/examples/quic_configure_link.py
@@ -41,48 +41,50 @@ The test reports
 | server    |     | limitation            |     | client    |
 +-----------+     +-----------------------+     +-----------+
 |  server_ip|     |                       |     |           |
-|           |     | midbox_if             |     |           |
+|           |     | up_iface   down_iface |     |           |
 +-----------+     +-----------------------+     +-----------+
 | entity:   |     | entity:               |     | entity:   |
 |  server   |     |  midbox (middle-box)  |     |  client   |
 +-----------+     +-----------------------+     +-----------+
 
 OpenBACH parameters:
- - post-processing-entity : entity where the post-processing will be performed
- - project_name : the name of the project
- - path : the path where the post processing data will be stored
+ - post-processing-entity: entity where the post-processing will be performed
+ - project_name: the name of the project
+ - path: the path where the post processing data will be stored
 
 Specific scenario parameters:
- - bandwidth_server_to_client : the bandwidth limitation in the
+ - bandwidth_server_to_client: the bandwidth limitation in the
      server to client direction
- - bandwidth_client_to_server : the bandwidth limitation in the
+ - bandwidth_client_to_server: the bandwidth limitation in the
      client to server direction
- - delay_server_to_client : the delay limitation in the
+ - delay_server_to_client: the delay to add in the
      server to client direction
- - delay_client_to_server : the delay limitation in the
+ - delay_client_to_server: the delay to add in the
      client to server direction
- - resources : resources to download in parallel over concurrent streams
+ - resources: resources to download in parallel over concurrent QUIC streams
 
 Other parameters:
- - server_ip : ip address of the server
- - midbox_if: Interface name on which the delay and/or bandwidth
-     limitation is introduced
-
+ - server_ip: ip address of the server
+ - up_iface: Network interface name of the middlebox entity on which the delay and/or bandwidth
+     limitation are applied in the client to server (uplink) direction 
+ - down_iface: Network interface name of the middlebox entity on which the delay and/or bandwidth
+     limitation are applied in the server to client (downlink) direction
+ 
 Step-by-step description of the scenario:
- - clean-midbox-if : clean the middle box interface
- - add-limit-if : add delay and/or bandwidth limitations in both
-     directions on midbox-if                                      
- - captures : capture network traffic to analyze
- - download : start the download of resources
- - analyzes:  analyze captured traffic
- - clean-midbox-if : clean the middle box interface
+ - teardown: clean the middle box interfaces
+ - setup: set the middle box interfaces to emulate uplink and downlink paths
+ - captures: capture network traffic to analyze
+ - download: start the download of resources
+ - analyzes: analyze captured traffic
+ - teardown: clean the middle box interface
 """
+
 import os
+import sys
 from scenario_builder import Scenario
 from auditorium_scripts.scenario_observer import ScenarioObserver, DataProcessor
 from scenario_builder.scenarios import network_configure_link, service_quic, transport_tcpdump
 from scenario_builder.helpers.service.quic import quic_find_client
-from scenario_builder.helpers.transport.tcpdump import tcpdump_find_analyze
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -107,7 +109,7 @@ def extract_tcpdump_statistic(job):
                          index=timestamps)
 
 
-def setup(scenario, name, entity, interfaces, mode, bandwidth=None, delay_distribution='normal', delay=0, jitter=0, loss_model='random', loss_model_params=[0.0], buffer_size=10000, wait_finished=None):
+def setup(scenario, name, entity, interfaces, mode, bandwidth=None, delay_distribution='normal', delay=0, jitter=0, loss_model='random', loss_model_params=[0.0], buffer_size=50000, wait_finished=None):
     sub = scenario.add_function('start_scenario_instance', wait_finished=wait_finished)
     sub.configure(network_configure_link.build(entity, interfaces, mode, 'apply', bandwidth, delay, jitter, delay_distribution, loss_model, loss_model_params, buffer_size, name))
     return sub
@@ -122,13 +124,13 @@ def service_quic_scenario(scenario, server, server_ip, server_port, server_imple
 
 def transport_tcpdump_capture_scenario(scenario, name, entity, iface, capture_file, src_ip=None, dst_ip=None, src_port=None, dst_port=None, proto=None, wait_finished=None):
     sub = scenario.add_function('start_scenario_instance', wait_finished=wait_finished)
-    sub.configure(transport_tcpdump.build(entity, 'capture', iface, capture_file, src_ip, dst_ip, src_port, dst_port, proto, scenario_name=name))
+    sub.configure(transport_tcpdump.build(entity, 'capture', capture_file, iface, src_ip, dst_ip, src_port, dst_port, proto, scenario_name=name))
     return sub
 
 
 def transport_tcpdump_analyze_scenario(scenario, name, entity, capture_file, src_ip=None, dst_ip=None, src_port=None, dst_port=None, proto=None, metrics_interval=None, post_processing_entity=None, wait_finished=None):
     sub = scenario.add_function('start_scenario_instance', wait_finished=wait_finished)
-    sub.configure(transport_tcpdump.build(entity, 'analyze', None, capture_file, src_ip, dst_ip, src_port, dst_port, proto, metrics_interval=metrics_interval, 
+    sub.configure(transport_tcpdump.build(entity, 'analyze', capture_file, None, src_ip, dst_ip, src_port, dst_port, proto, metrics_interval=metrics_interval, 
                                           post_processing_entity=post_processing_entity, scenario_name=name))
     return sub
 
@@ -147,12 +149,13 @@ def teardown(scenario, name, entity, interfaces, mode, wait_finished=None):
 
 def build(scenario, args, capture=False):
     # Add scenarios to clear interfaces
+    interfaces = ','.join([args.up_iface, args.down_iface])
     teardowns = [
        teardown(
           scenario, 
-          name='Clean {} on {} in {}'.format(args.middlebox_interfaces, args.entity, mode), 
+          name='Clean {} on {} in {}'.format(interfaces, args.entity, mode), 
           entity=args.entity, 
-          interfaces=args.middlebox_interfaces, 
+          interfaces=interfaces, 
           mode=mode
        )
        for mode in ('ingress', 'egress')
@@ -164,21 +167,23 @@ def build(scenario, args, capture=False):
           scenario, 
           name=name, 
           entity=args.entity, 
-          interfaces=args.middlebox_interfaces, 
+          interfaces=iface, 
           mode=mode, 
           bandwidth=bandwidth, 
-          delay=delay, 
+          delay=delay,
+          loss_model=args.loss_model,
+          loss_model_params=args.loss_model_parameters, 
           wait_finished=teardowns
        )
-       for name, mode, bandwidth, delay in (('Set downlink path', 'ingress', args.bandwidth_server_to_client, args.delay_server_to_client),
-                                            ('Set uplink path', 'egress', args.bandwidth_client_to_server, args.delay_client_to_server))
+       for name, iface, mode, bandwidth, delay in (('Set downlink path', args.down_iface, 'egress', args.bandwidth_server_to_client, args.delay_server_to_client),
+                                                   ('Set uplink path', args.up_iface, 'egress', args.bandwidth_client_to_server, args.delay_client_to_server))
     ]
     if capture:
        # Add scenario to capture packets
        captures = [
           transport_tcpdump_capture_scenario(
              scenario, 
-             name='Capture traffic {} by {}'.format(direction, entity), 
+             name='Capture traffic {} by the {}'.format(direction, entity), 
              entity=entity, 
              iface='any', 
              capture_file=os.path.join(args.pcaps_dir, '{}_{}_{}.pcap'.format(args.server_implementation, args.client_implementation, direction)), 
@@ -203,14 +208,15 @@ def build(scenario, args, capture=False):
           args.client,
           args.client_implementation,
           args.resources,
-          1,
+          args.nb_runs,
           args.download_dir,
           args.server_log_dir,
           args.server_extra_args,
           args.client_log_dir,
           args.client_extra_args,
           args.post_processing_entity,
-          wait_launched=captures if capture else None
+          wait_launched=captures if capture else None,
+          wait_finished=setups
        )
     ]
     if capture:
@@ -221,12 +227,12 @@ def build(scenario, args, capture=False):
        analyzes = [
           transport_tcpdump_analyze_scenario(
              scenario, 
-             name='Analyze traffic {} by {}'.format(direction, entity), 
+             name='Analyze traffic {} by the {}'.format(direction, entity), 
              entity=entity, 
              capture_file=os.path.join(args.pcaps_dir, '{}_{}_{}.pcap'.format(args.server_implementation, args.client_implementation, direction)), 
              **fields, 
              proto='udp', 
-             metrics_interval=100, 
+             metrics_interval=50, 
              post_processing_entity=args.post_processing_entity, 
              wait_finished=captures
           ) 
@@ -235,16 +241,17 @@ def build(scenario, args, capture=False):
                                             (args.client, 'received', {'src_ip':args.server_ip, 'src_port':args.server_port}),
                                             (args.client, 'sent', {'dst_ip':args.server_ip, 'dst_port':args.server_port}))
        ]
-    
+ 
        global TITLES
-       TITLES += ('{} received by server', '{} sent by server', '{} received by client', '{} sent by client')
+       TITLES += ('{} received by the server', '{} sent by the server', '{} received by the client', '{} sent by the client')
+
     # Add scenarios to clear interfaces
     teardowns = [
        teardown(
           scenario, 
-          name='Clean {} on {} in {})'.format(args.middlebox_interfaces, args.entity, mode), 
+          name='Clean {} on {} in {}'.format(interfaces, args.entity, mode), 
           entity=args.entity, 
-          interfaces=args.middlebox_interfaces, 
+          interfaces=interfaces, 
           mode=mode,
           wait_finished=downloads
           
@@ -258,27 +265,43 @@ def build(scenario, args, capture=False):
 def main(argv=None):
     observer = ScenarioObserver()
     observer.add_scenario_argument(
-            '--entity', '--configure-link-entity', '-e', required=True,
-            help='Name of the entity where configure link should run')
+            '--entity', '--middlebox-entity', '-e', required=True,
+            help='Name of the middlebox entity')
     observer.add_scenario_argument(
             '--bandwidth-server-to-client', '-B', required=True,
-            help='Bandwidth allocated for the server to answer the client')
+            help='Bandwidth limitation in the server to client direction '
+                 '(Kbps|Mbps|Gbps expressed as [value][K][M][G])')
     observer.add_scenario_argument(
             '--bandwidth-client-to-server', '-b', required=True,
-            help='Bandwidth allocated for the client to ask the server')
+            help='Bandwidth limitation in the client to server direction '
+                 '(Kbps|Mbps|Gbps expressed as [value][K][M][G])')
     observer.add_scenario_argument(
             '--delay-server-to-client', '-D', required=True, type=int,
-            help='Delay for a packet to go from the server to the client')
+            help='Delay to add in the server to client direction (ms)')
     observer.add_scenario_argument(
             '--delay-client-to-server', '-d', required=True, type=int,
-            help='Delay for a packet to go from the client to the server')
+            help='Delay to add in the client to server direction (ms)')
     observer.add_scenario_argument(
-            '--middlebox-interfaces', '--interfaces', '-m', required=True,
-            help='Comma-separated list of the network interfaces to emulate link on on the middlebox')
+            '--loss-model', '-lm', choices=['random', 'state', 'gemodel'],
+            default='random', help='Packets loss model to use (only for apply operation)')
+    observer.add_scenario_argument(
+            '--loss-model-parameters', '-lmp', default=0.0, type=float, nargs='*',
+            help='Packets loss model parameters to use (only for apply operation). Warning: '
+            'This must not be the last argument of the scenario'
+            'This could be used as follows:'
+            '-lm random -lmp 1; to add 1% random losses'
+            '-lm gemodel -lmp 1.8 64.5 100 0; to add P(g|g)= 0.982, P(g|b)= 0.645, P(b|b)= 0.355, P(b|g)= 0.018'
+            '(see [RFC6534] for more details)')
+    observer.add_scenario_argument(
+            '--up-iface', '-msi', required=True,
+            help='Network interface of the middlebox that is connected to the server')
+    observer.add_scenario_argument(
+            '--down-iface', '-mci', required=True,
+            help='Network interface of the middlebox that is connected to the client')
 
     observer.add_scenario_argument(
             '--server', '-s', required=True,
-            help='name of the entity on which to run QUIC server')
+            help='Name of the entity on which to run QUIC server')
     observer.add_scenario_argument(
             '--server-ip', '-A', required=True,
             help='The IP address of the QUIC server')
@@ -290,7 +313,7 @@ def main(argv=None):
             help='The QUIC implementation to run by the server. Possible values are: ngtcp2, picoquic, quicly')
     observer.add_scenario_argument(
             '--client', '-c', required=True,
-            help='name of the entity on which to run QUIC client')
+            help='Name of the entity on which to run QUIC client')
     observer.add_scenario_argument(
             '--client-implementation', '-i', required=True,
             help='The QUIC implementation to run by the client. Possible values are: ngtcp2, picoquic, quicly')
@@ -334,14 +357,14 @@ def main(argv=None):
     for run_number in range(args.nb_runs):
         # Built main scenario
         scenario = Scenario(name, 'Download resources using QUIC under a given network condition.'
-                                  ' Exchanged packets are captured and analyzed')
+                                  ' Exchanged packets are captured and then analyzed')
         build(scenario, args, capture=run_number==0)
         observer.launch_and_wait(scenario)
         results = DataProcessor(observer)
         quic_client, = scenario.extract_function_id(quic=quic_find_client, include_subscenarios=True)
         results.add_callback('download_time', extract_quic_statistic, quic_client)
         if run_number == 0:
-           tcpdump_analyzes = scenario.extract_function_id(tcpdump=tcpdump_find_analyze, include_subscenarios=True)
+           tcpdump_analyzes = scenario.extract_function_id("pcap_postprocessing", include_subscenarios=True)
            for title, tcpdump_analyze in zip(TITLES, tcpdump_analyzes):
                results.add_callback(title, extract_tcpdump_statistic, tcpdump_analyze)
 
@@ -350,8 +373,8 @@ def main(argv=None):
            for title, df in plots.items():
                if title in TITLES:
                   for stat_name, ts_xlabel, ts_ylabel, ts_title in (
-                      ('bit_rate', 'time (ms)', 'bit rate (Kbps)', title.format('bitRate')), 
-                      ('bytes_count', 'time (ms)', 'bytes count (B)', title.format('bytesCount'))): 
+                      ('bit_rate', 'Time (ms)', 'Bit Rate (Kbps)', title.format('Bit Rate')), 
+                      ('bytes_count', 'Time (ms)', 'Bytes Count (B)', title.format('Bytes Count'))): 
                       figure, axis = plt.subplots()
                       plt.xlabel(ts_xlabel)
                       plt.ylabel(ts_ylabel)
@@ -362,18 +385,24 @@ def main(argv=None):
 
         else: 
            plots = results.post_processing()
-        data = plots['download_time']
-        timestamps.append(data[0][0])
-        download_times.append(data[0][1])
+        try:
+           data = plots['download_time']
+           timestamps.append(data[0][0])
+           download_times.append(data[0][1])
+        except KeyError as ex:
+               message = ("No 'download_time' available meaning that no statistic when postprocessing quic scenario. "
+                          "May be the scenario failed")
+               print(message)
+               sys.exit(message)
         
     timestamps = [timestamp - min(timestamps) for timestamp in timestamps]
     df = pd.DataFrame({'download_time': download_times}, index=timestamps)
     for stat_name, ts_xlabel, ts_ylabel, ts_title, cdf_xlabel, cdf_ylabel, cdf_title in (
-        ('download_time', 'time (ms)', 'download time (ms)', 'Download Time TS', 'download time (ms)', 'CDF', 'Download Time CDF'),):
+        ('download_time', 'Time (ms)', 'Download Time (ms)', 'Download Time TS', 'Download Time (ms)', 'CDF', 'Download Time CDF'),):
         figure, axis = plt.subplots()
         plt.xlabel(ts_xlabel)
         plt.ylabel(ts_ylabel)
-        df.plot(y=stat_name, ax=axis, title=ts_title, grid=True, legend=False)
+        df.plot(y=stat_name, ax=axis, title=ts_title, grid=True, legend=False, linewidth=2)
         figure_name = '{}_{}_{}_ts.png'.format(args.server_implementation, args.client_implementation, 'download_time')
         figure_path = os.path.join(args.report_dir, figure_name)
         plt.savefig(figure_path)
