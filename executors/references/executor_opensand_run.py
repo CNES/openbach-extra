ebb12f53c89da7b4b0822f52801d143a63382306
Mathias Ettinger <mathias.ettinger@viveris.f>
2021-03-09 13:40:59 +0100

    Update OpenSAND job/reference executor

--- executors/references/executor_opensand_run.py
+++ executors/references/executor_opensand_run.py
@@ -36,25 +36,18 @@ from scenario_builder.scenarios import opensand_run
 
 
 class Entity:
-    def __init__(self, entity, opensand_id, emulation_ip, tap_name='opensand_tap'):
+    def __init__(self, entity, infrastructure, topology, profile):
         self.entity = entity
-        self.opensand_id = int(opensand_id)
-        self.emulation_ip = validate_ip(emulation_ip)
-        self.tap_name = tap_name
-
-
-class GatewayPhy:
-    def __init__(self, entity_phy, entity_net_acc, interco_phy, interco_net_access):
-        self.entity = entity_phy
-        self.net_acc_entity = entity_net_acc
-        self.interconnect_net_access = validate_ip(interco_net_access)
-        self.interconnect_phy = validate_ip(interco_phy)
+        self.infrastructure = infrastructure
+        self.topology = topology
+        self.profile = profile
 
 
 class Satellite:
-    def __init__(self, entity, emulation_ip):
+    def __init__(self, entity, infrastructure, topology):
         self.entity = entity
-        self.emulation_ip = validate_ip(emulation_ip)
+        self.infrastructure = infrastructure
+        self.topology = topology
 
 
 class ValidateSatellite(argparse.Action):
@@ -63,73 +56,30 @@ class ValidateSatellite(argparse.Action):
         setattr(args, self.dest, satellite)
 
 
-class ValidateTerminal(Validate):
+class ValidateGroundEntity(Validate):
     ENTITY_TYPE = Entity
-
-
-class ValidateGateway(Validate):
-    ENTITY_TYPE = Entity
-
-
-class ValidateGatewayPhy(Validate):
-    ENTITY_TYPE = GatewayPhy
-
-
-def validate_ip(ip):
-    return ipaddress.ip_address(ip).compressed
 
 
 def main(argv=None):
     observer = ScenarioObserver()
     observer.add_scenario_argument(
             '--satellite', '--sat', '-s', required=True, action=ValidateSatellite,
-            nargs=2, metavar=('ENTITY', 'EMULATION_ADDRESS'),
+            nargs=3, metavar=('ENTITY', 'INFRASTRUCTURE_PATH', 'TOPOLOGY_PATH'),
             help='The satellite of the platform. Must be supplied only once.')
     observer.add_scenario_argument(
-            '--gateway', '-gw', required=True, action=ValidateGateway, nargs=4,
-            metavar=('ENTITY', 'OPENSAND_ID', 'EMULATION_ADDRESS', 'TAP_NAME'),
-            help='A gateway in the platform. Must be supplied at least once.')
-    observer.add_scenario_argument(
-            '--gateway-phy', '-gwp', action=ValidateGatewayPhy,
-            nargs=4, metavar=('ENTITY_PHY', 'ENTITY_NET_ACC','INTERCONNECT_PHY_ADDRESS', 'INTERCONNECT_NET_ACC_ADDRESS'),
-            help='The physical part of a split gateway. Must reference the '
-            'net access part previously provided using the --gateway option. '
-            'Optional, can be supplied only once per gateway.')
-    observer.add_scenario_argument(
-            '--satellite-terminal', '-st', required=True, action=ValidateTerminal, nargs=4,
-            metavar=('ENTITY', 'OPENSAND_ID', 'EMULATION_ADDRESS', 'TAP_NAME'),
-            help='A satellite terminal in the platform. Must be supplied at least once.')
+            '--ground-entity', '--ground', '--entity', '-g', '-e',
+            required=True, action=ValidateGroundEntity, nargs=4,
+            metavar=('ENTITY', 'INFRASTRUCTURE_PATH', 'TOPOLOGY_PATH', 'PROFILE_PATH'),
+            help='A ground entity in the platform. Must be supplied at least once.')
     observer.add_scenario_argument(
             '--duration', '-d', type=int, default=0,
             help='Duration of the opensand run test, leave blank for endless emulation.')
 
     args = observer.parse(argv, opensand_run.SCENARIO_NAME)
 
-    run_gateways = []
-    phy_gateways = args.gateway_phy or []
-    for gateway in args.gateway:
-        for gateway_phy in phy_gateways:
-            if gateway.entity == gateway_phy.net_acc_entity:
-                run_gateways.append(opensand_run.SPLIT_GW(
-                        gateway.entity,
-                        gateway_phy.entity,
-                        gateway.opensand_id,
-                        gateway.emulation_ip,
-                        gateway_phy.interconnect_phy,
-                        gateway_phy.interconnect_net_access,
-                        gateway.tap_name))
-                break
-        else:
-            run_gateways.append(opensand_run.GW(
-                    gateway.entity,
-                    gateway.opensand_id,
-                    gateway.emulation_ip,
-                    gateway.tap_name))
-
     scenario = opensand_run.build(
             args.satellite,
-            run_gateways,
-            args.satellite_terminal,
+            args.ground_entity or [],
             args.duration,
             args.scenario_name)
     observer.launch_and_wait(scenario)
