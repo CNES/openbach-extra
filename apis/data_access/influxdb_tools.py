ce80bfb99a53887a6524ee1dca9d768fcbe692d4
Mathias Ettinger <mathias.ettinger@viveris.fr>
2021-03-31 15:32:31 +0200

    Introduce timeouts in the data_access API calls

--- apis/data_access/influxdb_tools.py
+++ apis/data_access/influxdb_tools.py
@@ -395,6 +395,8 @@ def line_protocol(job_name, scenario_id, owner_id, agent_name, job_id, suffix, s
 class InfluxDBCommunicator:
     """Manage network access to an InfluxDB server"""
 
+    TIMEOUT = (2, 3600)  # Requests (connection, data) timeouts in second
+
     def __init__(self, ip, port=8086, db_name='openbach', precision='ms'):
         """Configure the routes to send/get data to/from InfluxDB"""
 
@@ -410,11 +412,11 @@ class InfluxDBCommunicator:
 
     def sql_query(self, query):
         """Send a query to InfluxDB and gather the results"""
-        return requests.get(self.querying_URL, params={'q': query}).json()
+        return requests.get(self.querying_URL, params={'q': query}, timeout=self.TIMEOUT).json()
 
     def data_write(self, data):
         """Send data to InfluxDB so they are stored"""
-        return requests.post(self.writing_URL, data.encode())
+        return requests.post(self.writing_URL, data.encode(), timeout=self.TIMEOUT)
 
 
 class InfluxDBConnection(InfluxDBCommunicator):
