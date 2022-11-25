3465ade095dc8cf5b3e2ae962b8d1d929ba0d1cd
Mathias Ettinger <mathias.ettinger@viveris.fr>
2022-11-25 17:42:30 +0100

    [Jobs] Remove unnecessary setuptools dependencies in installs

--- apis/setup.py
+++ apis/setup.py
@@ -27,12 +27,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see http://www.gnu.org/licenses/.
 
-import os
+from pathlib import Path
 from setuptools import setup, find_packages
 
 
 def read(fname):
-    with open(os.path.join(os.path.dirname(__file__), fname)) as readme:
+    with open(Path(__file__).parent / fname) as readme:
         return readme.read()
 
 
