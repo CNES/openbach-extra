8369ab3c3bfe6e8890f027fe6b74b0b43d001833
Mathias Ettinger <mathias.ettinger@viveris.fr>
2025-02-18 11:06:52 +0100

    [Jobs] Fix jobs on noble numbat

--- externals_jobs/stable_jobs/transport/pep/files/pep.py
+++ externals_jobs/stable_jobs/transport/pep/files/pep.py
@@ -125,43 +125,69 @@ def set_conf(ifaces, src_ip, dst_ip, port, mark, table_num, unset=False):
         manage_rule(target_chain, unset, port, mark, ip_dst=ip)
 
 
-def main(ifaces, src_ip, dst_ip, stop, port, addr, fopen, maxconns,
-         gcc_interval, log_file, pending_time, mark, table_num):
+def main(
+        interfaces, source, destination, stop, port, fastopen,
+        nodelay, quickack, cork, mss, congestion_control, syn_sniffer,
+        maxconns, gc_interval, log_file, pending_lifetime, mark, table_num):
+    # TODO use syn_sniffer in 'set_conf' and pass it to 'cmd'
+
+    ifaces = re.findall(r'[^\,\ \t]+', interfaces)
+    src_ip = re.findall(r'[^\,\ \t]+', source)
+    dst_ip = re.findall(r'[^\,\ \t]+', destination)
+
     if stop:
         # unset routing configuration
         set_conf(ifaces, src_ip, dst_ip, port, mark, table_num, unset=True)
         run_command(['systemctl', 'restart', 'pepsal.service'])
+        return
+
+    # set routing conf
+    set_conf(ifaces, src_ip, dst_ip, port, mark, table_num)
+
+    # stop pepsal service
+    run_command(['systemctl', 'stop', 'pepsal.service'])
+    collect_agent.send_log(syslog.LOG_DEBUG, 'pepsal.service stopped')
+
+    # launch pepsal
+    cmd = [
+            'pepsal',
+            '-p', str(port),
+            '-c', str(maxconns),
+            '-g', str(gc_interval),
+            '-l', str(log_file),
+            '-t', str(pending_lifetime),
+    ]
+    if fastopen:
+        cmd.append('-f')
+    if nodelay:
+        cmd.append('-n')
+    if quickack:
+        cmd.append('-q')
+    if cork:
+        cmd.append('-k')
+    if mss:
+        cmd.append('-m')
+        cmd.append(str(mss))
+    if congestion_control:
+        cmd.append('-C')
+        cmd.append(str(congestion_control))
+
+    try:
+        p = subprocess.run(cmd, stderr=subprocess.PIPE, stdout=subprocess.DEVNULL)
+    except Exception as ex:
+        set_conf(ifaces, src_ip, dst_ip, port, mark, table_num, unset=True)
+        message = 'ERROR: {}'.format(ex)
+        collect_agent.send_log(syslog.LOG_ERR, message)
+        sys.exit(message)
+
+    if p.returncode:
+        set_conf(ifaces, src_ip, dst_ip, port, mark, table_num, unset=True)
+        message = 'ERROR: {} exited with non-zero return value ({}): {}'.format(
+            cmd, p.returncode, p.stderr.decode())
+        collect_agent.send_log(syslog.LOG_ERR, message)
+        sys.exit(message)
     else:
-        # set routing conf
-        set_conf(ifaces, src_ip, dst_ip, port, mark, table_num)
-
-        # stop pepsal service
-        run_command(['systemctl', 'stop', 'pepsal.service'])
-        collect_agent.send_log(syslog.LOG_DEBUG, 'pepsal.service stopped')
-
-        # launch pepsal
-        cmd = [
-                'pepsal', str(fopen), '-p', str(port),
-                '-a', str(addr), '-c', str(maxconns),
-                '-g', str(gcc_interval), '-l', str(log_file),
-                '-t', str(pending_time),
-        ]
-        try:
-            p = subprocess.run(cmd, stderr=subprocess.PIPE, stdout=subprocess.DEVNULL)
-        except Exception as ex:
-            set_conf(ifaces, src_ip, dst_ip, port, mark, table_num, unset=True)
-            message = 'ERROR: {}'.format(ex)
-            collect_agent.send_log(syslog.LOG_ERR, message)
-            sys.exit(message)
-
-        if p.returncode:
-            set_conf(ifaces, src_ip, dst_ip, port, mark, table_num, unset=True)
-            message = 'ERROR: {} exited with non-zero return value ({}): {}'.format(
-                cmd, p.returncode, p.stderr.decode())
-            collect_agent.send_log(syslog.LOG_ERR, message)
-            sys.exit(message)
-        else:
-            collect_agent.send_log(syslog.LOG_DEBUG, 'PEP launched successfully')
+        collect_agent.send_log(syslog.LOG_DEBUG, 'PEP launched successfully')
 
 
 def command_line_parser():
@@ -169,25 +195,40 @@ def command_line_parser():
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-p', '--port', type=int, default=5000,
             help='The port PEPsal uses to listen for incoming connection')
-    parser.add_argument('-a', '--address', type=str, default='0.0.0.0',
-            help='The address PEPsal uses to bind the listening socket')
     parser.add_argument('-f', '--fastopen', action='store_true',
-            help='Enable TCP FastOpen')
+            help='Enable TCP FastOpen on outgoing sockets')
+    parser.add_argument('-n', '--nodelay', action='store_true',
+            help='Enable TCP no delay on outgoing sockets')
+    parser.add_argument('-q', '--quickack', action='store_true',
+            help='Enable TCP quick ACK on outgoing sockets')
+    parser.add_argument('-k', '--cork', action='store_true',
+            help='Enable TCP CORK option on outgoing sockets')
+    parser.add_argument('-M', '--mss', type=int,
+            help='Enable TCP maximum segment size option on '
+            'outgoing socket and set it to <mss> bytes')
+    parser.add_argument('-C', '--congestion-control',
+            help='The name of the TCP congestion control '
+            'algorithm to use on outgoing sockets')
+    parser.add_argument('-S', '--syn-sniffer',
+            help='name of an interface to sniff and extract ethernet or IP options '
+            'from SYN packets in order to replicate them on outgoing sockets')
     parser.add_argument('-c', '--maxconns', type=int, default=2112,
             help='The maximum number of simultaneous connections')
-    parser.add_argument('-g', '--gcc-interval', type=int, default=54000,
+    parser.add_argument('-g', '--gc-interval', type=int, default=54000,
             help='The garbage collector interval')
-    parser.add_argument('-l', '--log-file', type=str, default='/var/log/pepsal/connections.log',
-            help='The connections log path')
     parser.add_argument('-t', '--pending-lifetime', type=int, default=18000,
             help='The pending connections lifetime')
+    parser.add_argument('-l', '--log-file', type=str, default='/var/log/pepsal/connections.log',
+            help='The connections log path')
     parser.add_argument('-x', '--stop', action='store_true',
             help='If set, unset routing configuration')
-    parser.add_argument('-i', '--redirect-ifaces', type=str, default='',
-            help="Redirect all traffic from incoming interfaces to PEPsal (admits multiple interfaces separated by ',' ' ')")
-    parser.add_argument('-s', '--redirect-src-ip', type=str, default='',
+    parser.add_argument('-i', '--redirect-ifaces',
+            type=str, default='', dest='interfaces',
+            help="Redirect all traffic from incoming interfaces to '
+            'PEPsal (admits multiple interfaces separated by ',' ' ')")
+    parser.add_argument('-s', '--redirect-src-ip', type=str, default='', dest='source',
             help="Redirect all traffic with src IP to PEPsal (admits multiple IPs separated by ',' ' ')")
-    parser.add_argument('-d', '--redirect-dst-ip', type=str, default='',
+    parser.add_argument('-d', '--redirect-dst-ip', type=str, default='', dest='destination',
             help="Redirect all traffic with dest IP to PEPsal (admits multiple IPs separated by ',' ' ')")
     parser.add_argument('-m', '--mark', type=int, default=1,
             help='The mark used for routing packets to the PEP')
@@ -199,19 +240,4 @@ def command_line_parser():
 if __name__ == '__main__':
     with collect_agent.use_configuration('/opt/openbach/agent/jobs/pep/pep_rstat_filter.conf'):
         args = command_line_parser().parse_args()
-        port = args.port
-        addr = args.address
-        fopen = '-f' if args.fastopen else ''
-        maxconns = args.maxconns
-        gcc_interval = args.gcc_interval
-        log_file = args.log_file
-        pending_time = args.pending_lifetime
-        ifaces = re.findall(r'[^\,\ \t]+', args.redirect_ifaces)
-        src_ip = re.findall(r'[^\,\ \t]+', args.redirect_src_ip)
-        dst_ip = re.findall(r'[^\,\ \t]+', args.redirect_dst_ip)
-        stop = args.stop
-        mark = args.mark
-        table_num = args.table_num
-
-        main(ifaces, src_ip, dst_ip, stop, port, addr, fopen, maxconns,
-             gcc_interval, log_file, pending_time, mark, table_num)
+        main(**vars(args))
